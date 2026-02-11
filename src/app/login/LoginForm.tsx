2118921edd6a1b604a6060f5a2aec7d4787e7dbe
flocom <valentin.halgand@gmail.com>
2026-02-11 13:41:58 +0100

    rebranding

--- src/app/login/LoginForm.tsx
+++ src/app/login/LoginForm.tsx
@@ -35,7 +35,7 @@ export function LoginForm() {
       <Icon size="lg">
         <Logo />
       </Icon>
-      <Heading>umami</Heading>
+      <Heading>Iselia Analytics</Heading>
       <Form onSubmit={handleSubmit} error={getErrorMessage(error)}>
         <FormField
           label={formatMessage(labels.username)}
